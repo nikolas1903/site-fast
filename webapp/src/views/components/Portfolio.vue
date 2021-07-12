224071434f250bca4f24c2920cd9682491264e35
Nikolas <nikolas.silva@oti.inf.br>
2021-07-12 03:23:08 -0300

    somente criação da aba Portfolio.

---
+++ webapp/src/views/components/Portfolio.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="profile-page">
+    <section class="section-profile-cover section-shaped my-0">
+      <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+    <div class="container container-lg">
+            <div class="row">
+                <div class="col-md-6 mb-5 mb-md-0">
+                    <div class="card card-lift--hover shadow border-0">
+                        <a href="https://google.com" title="Landing Page">
+                            <img v-lazy="'img/theme/landing.jpg'" class="card-img">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-5 mb-lg-0">
+                    <div class="card card-lift--hover shadow border-0">
+                        <router-link to="/profile" title="Profile Page">
+                            <img v-lazy="'img/theme/profile.jpg'" class="card-img">
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+<br><br>
+        <div class="container container-lg">
+            <div class="row">
+                <div class="col-md-6 mb-5 mb-md-0">
+                    <div class="card card-lift--hover shadow border-0">
+                        <router-link to="/landing" title="Landing Page">
+                            <img v-lazy="'img/theme/landing.jpg'" class="card-img">
+                        </router-link>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-5 mb-lg-0">
+                    <div class="card card-lift--hover shadow border-0">
+                        <router-link to="/profile" title="Profile Page">
+                            <img v-lazy="'img/theme/profile.jpg'" class="card-img">
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+  </div>
+</template>
+
+
+
+
+<script>
+import Tabs from "@/components/Tabs/Tabs.vue";
+import TabPane from "@/components/Tabs/TabPane.vue";
+export default {
+  components: {
+    Tabs,
+    TabPane,
+  },
+};
+</script>
+<style>
+</style>
