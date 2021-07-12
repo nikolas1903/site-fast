123b85b1ae8f36ece8445ebc0394f63788060b3d
Nikolas <nikolas.silva@oti.inf.br>
2021-07-12 03:24:42 -0300

    adição da header no portfólio

--- webapp/src/router.js
+++ webapp/src/router.js
@@ -42,7 +42,7 @@ export default new Router({
           path: "/portfolio",
           name: "components",
           components: {
-            
+            header: AppHeader,
             default: Portfolio,
             footer: AppFooter
           }
